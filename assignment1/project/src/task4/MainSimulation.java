33ef3098e898c5e46dce62c60f8eba209e47e926
Joel Bångdal <j.bangdal@gmail.com>
2020-04-28 20:54:01 +0200

    task4

--- assignment1/project/src/task4/MainSimulation.java
+++ assignment1/project/src/task4/MainSimulation.java
@@ -5,20 +5,27 @@ import java.io.*;
 public class MainSimulation extends GlobalSimulation {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println(String.format("%16s|%16s|%16s", "Q1 arrival time", "Mean Q2 size", "Rejected chance"));
+
+		int measurements = 1000;
+
 		Event actEvent;
-		State actState = new State(); // The state that shoud be used
+		State actState = new State(1); // The state that shoud be used
 		// Some events must be put in the event list at the beginning
-		insertEvent(ARRIVAL, 0);
-		insertEvent(MEASURE, 5);
+		eventList = new EventListClass();
+		insertEvent(ARRIVALQ1, 0);
+		insertEvent(MEASURE, actState.expRandom(5));
 
 		// The main simulation loop
-		while (time < 5000) {
+		while (actState.noMeasurements < measurements) {
 			actEvent = eventList.fetchEvent();
 			time = actEvent.eventTime;
 			actState.treatEvent(actEvent);
 		}
 
 		// Printing the result of the simulation, in this case a mean value
-		System.out.println(1.0 * actState.accumulated / actState.noMeasurements);
+		System.out.println(String.format("%16.2f|%16.2f|%15.2f%%"));
+	
+
 	}
 }
--- assignment1/project/src/task4/State.java
+++ assignment1/project/src/task4/State.java
@@ -4,27 +4,66 @@ import java.util.*;
 
 class State extends GlobalSimulation {
 
+	public final double arrTime;
+
+	int servers = 1000;
+	int serviceTime = 100;
+	int processRate = 8;
+	int measureTime = 1;
+
+	public State(double arrTime) {
+		this.arrTime = arrTime;
+	}
+
 	// Here follows the state variables and other variables that might be needed
 	// e.g. for measurements
-	public int numberInQueue = 0, accumulated = 0, noMeasurements = 0;
+	public int numberInQueue1 = 0, numberInQueue2, accumulated = 0, arrived = 0, rejected = 0, noMeasurements = 0;
 
 	Random slump = new Random(); // This is just a random number generator
+
+	public double expRandom(double mean) {
+		return -mean * Math.log(1 - slump.nextDouble());
+	}
+
+	public String eventTypeToString(Event x) {
+		switch (x.eventType) {
+			case ARRIVALQ1:
+				return "ARRIVALQ1";
+			case DEPARTQ1:
+				return "DEPARTQ1";
+			case DEPARTQ2:
+				return "DEPARTQ2";
+			case MEASURE:
+				return "MEASURE";
+			default:
+				return "Uknown";
+		}
+	}
+
+	public void printInfo(Event x) {
+		System.out.println(String.format("%10s %5.2f %4d %4d %4d %4d", eventTypeToString(x), x.eventTime,
+				numberInQueue1, numberInQueue2, arrived, rejected));
+	}
 
 	// The following method is called by the main program each time a new event has
 	// been fetched
 	// from the event list in the main loop.
 	public void treatEvent(Event x) {
 		switch (x.eventType) {
-			case ARRIVAL:
-				arrival();
+			case ARRIVALQ1:
+				arrivalq1();
 				break;
-			case READY:
-				ready();
+			case DEPARTQ1:
+				departq1();
+				break;
+			case DEPARTQ2:
+				departq2();
 				break;
 			case MEASURE:
 				measure();
 				break;
 		}
+		// printInfo(x);
 	}
 
 	// The following methods defines what should be done when an event takes place.
@@ -33,22 +72,37 @@ class State extends GlobalSimulation {
 	// a method if
 	// things are getting more complicated than this.
 
-	private void arrival() {
-		if (numberInQueue == 0)
-			insertEvent(READY, time + 2 * slump.nextDouble());
-		numberInQueue++;
-		insertEvent(ARRIVAL, time + 2.5 * slump.nextDouble());
+	private void arrivalq1() {
+		arrived++;
+		if (numberInQueue1 < 10) { // ACCEPT
+			numberInQueue1++;
+			if (numberInQueue1 == 1)
+				insertEvent(DEPARTQ1, time + expRandom(2.1));
+		} else { // REJECT
+			rejected++;
+		}
+		insertEvent(ARRIVALQ1, time + arrTime);
 	}
 
-	private void ready() {
-		numberInQueue--;
-		if (numberInQueue > 0)
-			insertEvent(READY, time + 2 * slump.nextDouble());
+	private void departq1() {
+		numberInQueue1--;
+		if (numberInQueue1 > 0)
+			insertEvent(DEPARTQ1, time + expRandom(2.1));
+
+		numberInQueue2++;
+		if (numberInQueue2 == 1)
+			insertEvent(DEPARTQ2, time + 2);
+	}
+
+	private void departq2() {
+		numberInQueue2--;
+		if (numberInQueue2 > 0)
+			insertEvent(DEPARTQ2, time + 2);
 	}
 
 	private void measure() {
-		accumulated = accumulated + numberInQueue;
+		accumulated = accumulated + numberInQueue2;
 		noMeasurements++;
-		insertEvent(MEASURE, time + 10 * slump.nextDouble());
+		insertEvent(MEASURE, time + measureTime);
 	}
 }
