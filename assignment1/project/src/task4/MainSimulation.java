40ab66f98c345c3e455d072b54242d4afb568697
Joel Bångdal <j.bangdal@gmail.com>
2020-04-28 23:50:37 +0200

    nästan klar med task 4

--- assignment1/project/src/task4/MainSimulation.java
+++ assignment1/project/src/task4/MainSimulation.java
@@ -1,6 +1,7 @@
 package task4;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class MainSimulation extends GlobalSimulation {
 
@@ -10,10 +11,17 @@ public class MainSimulation extends GlobalSimulation {
 		int[] x = {100, 10, 200, 10, 10, 10};
 		int[] lambda = {8, 80, 4, 4, 4, 4};
 		int[] T = {1, 1, 1, 4, 1, 4};
+		int average;
+		int[] datapoints;
+		double standardDeviation;
 
 		Event actEvent;
 		State actState;
+		System.out.println(String.format("%20s|%20s", "Mean", "Standard Deviation"));
 		for (int i = 0; i < T.length; i++) {
+			average = 0;
+			standardDeviation = 0.0;
+			datapoints = new int[M[i]];
 				
 			actState = new State(N[i], x[i], lambda[i], T[i]); // The state that shoud be used
 			// Some events must be put in the event list at the beginning
@@ -30,11 +38,20 @@ public class MainSimulation extends GlobalSimulation {
 				time = actEvent.eventTime;
 				actState.treatEvent(actEvent);
 				if (actEvent.eventType == MEASURE) {
+					average += actState.currentNbr;
 					sb.append(actState.currentNbr + "\n");
+					datapoints[actState.noMeasurements-1] = actState.currentNbr;
 				}
 			}
 			fw.write(sb.toString());
 			fw.close();
+			for (int j = 0; j < datapoints.length; j++) {
+				datapoints[j] = (datapoints[j]-(average/M[i])) * (datapoints[j]-(average/M[i]));
+			}
+			for (int j = 0; j < datapoints.length; j++) {
+				standardDeviation += datapoints[j];
+			}
+			System.out.println(String.format("%20d|%20f", (average/M[i]), Math.sqrt(standardDeviation/(average/M[i]))));
 		}
 	}
 }
