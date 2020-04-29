228d94141c09de71338cbc3fe4105ad67d9979c9
Joel Bångdal <j.bangdal@gmail.com>
2020-04-29 12:42:29 +0200

    lit

--- assignment1/project/src/task6/State.java
+++ assignment1/project/src/task6/State.java
@@ -3,16 +3,17 @@ package task6;
 import java.util.*;
 
 class State extends GlobalSimulation {
+	public int minutesInDay;
+	public double currentTime = 0;
+	public int waiting = 1;
+	public int arrivals = 0;
 
-	public final double arrTime;
-
-	public State(double arrTime) {
-		this.arrTime = arrTime;
+	public State(int minutesInDay) {
+		this.minutesInDay = minutesInDay;
 	}
 
 	// Here follows the state variables and other variables that might be needed
 	// e.g. for measurements
-	public int numberInQueue1 = 0, numberInQueue2, accumulated = 0, arrived = 0, rejected = 0, noMeasurements = 0;
 
 	Random slump = new Random(); // This is just a random number generator
 
@@ -20,39 +21,16 @@ class State extends GlobalSimulation {
 		return -mean * Math.log(1 - slump.nextDouble());
 	}
 
-	public String eventTypeToString(Event x) {
-		switch (x.eventType) {
-			case ARRIVALQ1:
-				return "ARRIVALQ1";
-			case DEPARTQ1:
-				return "DEPARTQ1";
-			case DEPARTQ2:
-				return "DEPARTQ2";
-			case MEASURE:
-				return "MEASURE";
-			default:
-				return "Uknown";
-		}
-	}
-
-	public void printInfo(Event x) {
-		System.out.println(String.format("%10s %5.2f %4d %4d %4d %4d", eventTypeToString(x), x.eventTime,
-				numberInQueue1, numberInQueue2, arrived, rejected));
-	}
-
 	// The following method is called by the main program each time a new event has
 	// been fetched
 	// from the event list in the main loop.
 	public void treatEvent(Event x) {
 		switch (x.eventType) {
-			case ARRIVALQ1:
-				arrivalq1();
+			case ARRIVAL:
+				arrival();
 				break;
-			case DEPARTQ1:
-				departq1();
-				break;
-			case DEPARTQ2:
-				departq2();
+			case DEPART:
+				depart();
 				break;
 			case MEASURE:
 				measure();
@@ -67,37 +45,35 @@ class State extends GlobalSimulation {
 	// a method if
 	// things are getting more complicated than this.
 
-	private void arrivalq1() {
-		arrived++;
-		if (numberInQueue1 < 10) { // ACCEPT
-			numberInQueue1++;
-			if (numberInQueue1 == 1)
-				insertEvent(DEPARTQ1, time + expRandom(2.1));
-		} else { // REJECT
-			rejected++;
+	private void arrival() {
+		currentTime = time;
+		arrivals++;
+		double temp1 = time + expRandom(15);
+		if(temp1 <= minutesInDay){
+			waiting ++;
+			insertEvent(ARRIVAL, temp1);
 		}
-		insertEvent(ARRIVALQ1, time + arrTime);
+		if(handling == 0 && waiting > 0){
+			insertEvent(DEPART, time + ((slump.nextDouble() * 10) + 10));
+			handling ++;
+			waiting--;
+		}
 	}
 
-	private void departq1() {
-		numberInQueue1--;
-		if (numberInQueue1 > 0)
-			insertEvent(DEPARTQ1, time + expRandom(2.1));
-
-		numberInQueue2++;
-		if (numberInQueue2 == 1)
-			insertEvent(DEPARTQ2, time + 2);
-	}
-
-	private void departq2() {
-		numberInQueue2--;
-		if (numberInQueue2 > 0)
-			insertEvent(DEPARTQ2, time + 2);
+	private void depart() {
+		handling--;
+		currentTime = time;
+		if (handling == 0 && waiting > 0){
+			insertEvent(DEPART, time + ((slump.nextDouble() * 10) + 10));
+			handling++;
+			waiting--;
+		} else {
+			insertEvent(MEASURE, time);
+		}
 	}
 
 	private void measure() {
-		accumulated = accumulated + numberInQueue2;
-		noMeasurements++;
-		insertEvent(MEASURE, time + expRandom(5));
+		currentTime = time;
+		insertEvent(MEASURE, time + 1);
 	}
 }
