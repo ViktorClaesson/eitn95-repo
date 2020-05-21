bd6d8c5657d465d6c48e0a3a5fbf3d90af0f3892
Viktor Claesson <claesson.viktor@gmail.com>
2020-05-21 10:03:46 +0200

    smol change

--- assignment2/project/src/task2/Task2.java
+++ assignment2/project/src/task2/Task2.java
@@ -1,15 +1,17 @@
 package task2;
 
 import sim.*;
+import java.util.stream.IntStream;
 
 public class Task2 {
 
 	public static void run() {
 		// Init the simulation
-		Proc q = new Square(0, 0);
+		IntStream s = IntStream.generate(() -> 0);
+		s.forEach(x -> System.out.println(x));
 
 		// Send init signals
-		Global.SendSignal(Signal.Type.TEST, q, 0);
+		Global.SendSignal(Signal.Type.TEST, new Square(0, 0), 0);
 
 		// Run simulation
 		Global.advanceUntil(() -> Global.time() > 1000);
